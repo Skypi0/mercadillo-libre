30ec20f5cff412300ad5ecf4bce18aa9ce1f7433
skypi0 <Xskypi0@gmail.com>
2024-05-20 20:44:40 -0600

    Basic components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistroComponent } from "./registro/registro.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, RegistroComponent, HomeComponent, LoginComponent]
 })
 export class AppComponent {
   title = 'mercadillo-libre';
